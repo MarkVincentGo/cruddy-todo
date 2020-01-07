eec3842b0e3c912005dccfeb22028547351cf5dd
MarkVincentGo <mark.vince.go@gmail.com>
2020-01-07 15:23:38 -0800

    Refactored code to remove extra code and comments

--- datastore/index.js
+++ datastore/index.js
@@ -12,171 +12,71 @@ exports.create = (text, callback) => {
     if (!err) {
       fs.writeFile(`${path.join(exports.dataDir, id)}.txt`, `${text}`, (err, data) => {
         if (err) {
-          console.log('ERROR: could not write file', `${text}`, `./data/${id}`);
+          // console.log('ERROR: could not write file', `${text}`, `./data/${id}`);
         } else {
-          // console.log(id)
           callback(null, {id, text});
-          //console.log('SUCCESS', `${text}`, `./data/${id}`);
         }
       });
-
     }
-  //console.log(items);
   });
-
 };
-
-
 
 
 exports.readAll = (callback) => {
+  //directoryArr is the array of files contained in datastore/data
   var directoryArr = fs.readdirSync(exports.dataDir);
-  // var directoryContents = fs.readFile(path.join(exports.dataDir, directory)
-
-  // var mapped = _.map(directoryArr, (singleElement) => {
-  //   var id = singleElement.slice(0, 5);
-  //   var text = '';
-  //   var readFile = (callback) => {
-
-  //     fs.readFile(path.join(exports.dataDir, singleElement), (err, fileData) => {
-  //       if (err) {
-  //         callback(null, 'no file found');
-  //       } else {
-  //         //console.log('callback initiated');
-  //         callback(null, fileData.toString());
-  //       }
-  //     });
-  //   };
-
-  //   return ({id: id, text: readFile((err, data) => {
-  //     if (!err) {
-  //       console.log(data);
-  //       return data;
-  //     }
-  //   })});
-
-  //   // readCount((err, data) => {
-  //   //   return
-  //   // })
-
-  //   //return ({id: singleElement.slice(0, 5), text: contents.toString()});
-  //   // return {{id: '00009', text: 'something'}}
-
-  // });
-  // console.log(mapped);
-  // callback(null, mapped);
 
   var mapped = [];
-  if (directoryArr.length === 0) {
-    callback(null, []);
-  } else {
+
   // iterate through because I dont know how to do it with map
-    for (var i = 0; i < directoryArr.length; i++) {
-      // gives string of array
-      var id = directoryArr[i].slice(0, 5);
-      mapped.push({id: id, text: id});
-      // fs.readFile(path.join(exports.dataDir, directoryArr[i]), (err, fileData) => {
-      //   if (err) {
-      //     callback(null, 'no file found');
-      //   } else {
-      //     mapped.push({id, text: fileData.toString()});
-      //     if (mapped.length === directoryArr.length) {
-      //       callback(null, mapped);
-      //     }
-      //   }
-      // });
-    }
-    callback(null, mapped);
+  for (var i = 0; i < directoryArr.length; i++) {
+    // gives string of array
+    var fileName = directoryArr[i].slice(0, 5);
+    mapped.push({id: fileName, text: fileName});
+
   }
-  // function readFile(callback) {
-
-  // }
-  // console.log(mapped);
-  // callback(null, mapped);
-
-
-
-
-  // console.log(directory); =
-
-  // var mapped = (directoryArr, directoryContents)  //returns text: 'something'
-  //returns id: '00009'
-
-
-  //  _.map(list, iteratee, [context]) Alias: collect
-  // Produces a new array of values by mapping each value in list through a transformation function (iteratee). The iteratee is passed three arguments: the value, then the index (or key) of the iteration, and finally a reference to the entire list.
-
-  // _.map([1, 2, 3], function(num){ return num * 3; });
-  // => [3, 6, 9]
-
-
-  // ['00009.txt', '00010.txt'];
-  // [{id: '00009', text: 'something'}]
-
-  //we might need to use the readFile method to extract the full value
-  //of each todo in the array, and return it.
-
-
-  // fs.readFile
-
-
-  // var data = _.map(items, (text, id) => {
-  //   return { id, text };
-  // });
-  // callback(null, data);
+  callback(null, mapped);
 
 };
 
+
 exports.readOne = (id, callback) => {
-  var directoryArr = fs.readdirSync(exports.dataDir);
-  var target;
-  _.each(directoryArr, (todo) => {
-    if (todo.slice(0, 5) === id) {
-      target = todo;
-    }
-  });
-
-  if (target === undefined) {
-    target = 'something';
-  }
-
-  fs.readFile(path.join(exports.dataDir, target), (err, data) => {
+  fs.readFile(path.join(exports.dataDir, `${id}.txt`), (err, data) => {
     if (err) {
       callback(new Error(`No item with id: ${id}`));
     }
     if (!err) {
-      callback(null, { id: target.slice(0, 5), text: data.toString() });
+      callback(null, { id: id, text: data.toString() });
     }
   });
-
-  // var text = items[id];
-  // if (!text) {
-  //   callback(new Error(`No item with id: ${id}`));
-  // } else {
-  //   callback(null, { id, text });
-  // }
 };
 
+
+
+
+
+
+
+
 exports.update = (id, text, callback) => {
-  var directoryArr = fs.readdirSync(exports.dataDir);
-  var target;
-  _.each(directoryArr, (todo) => {
-    if (todo.slice(0, 5) === id) {
-      target = todo;
-    }
-  });
+  // var directoryArr = fs.readdirSync(exports.dataDir);
+  // var target;
+  // _.each(directoryArr, (todo) => {
+  //   if (todo.slice(0, 5) === id) {
+  //     target = todo;
+  //   }
+  // });
 
-
-  if (target) {
-
-    fs.writeFile(path.join(exports.dataDir, target), text, (err, data) => {
+  if ((fs.readdirSync(exports.dataDir)).includes(`${id}.txt`)) {
+    fs.writeFile(path.join(exports.dataDir, `${id}.txt`), text, (err, data) => {
       if (err) {
         callback(new Error(`No item with id: ${id}`));
       } else {
         callback(null, {id, text});
       }
     });
-  } else {
+  }
+  else {
     callback(new Error(`No item with id: ${id}`));
   }
 
